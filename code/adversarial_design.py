d27dd6c0f420cdd283f3ad408e67ba037b2098db
MauricioCafiero <cafierom@gmail.com>
2026-03-13 15:16:40 +0000

    initial code for adversarial design

---
+++ code/adversarial_design.py
@@ -0,0 +1,203 @@
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from google.colab import userdata
+from langchain_core.tools import tool
+from langgraph.graph import START, StateGraph
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+import gradio as gr
+import os
+from PIL import Image
+from collections import Counter
+from typing import Annotated, TypedDict
+import time, sys
+from anthropic import Anthropic
+
+sys.path.append('MolecularPropertyOptimization/code')
+from MolPropOp import *
+from docking_module import *
+
+tools = [grow_cycle, replace_groups, make_random_list]
+
+anthropic_key = os.getenv("ANTHROPIC_KEY")
+client = Anthropic(api_key=anthropic_key)
+
+openai_key = os.getenv("OPENAI_API_KEY")
+model = ChatOpenAI(model_name="gpt-5.2", api_key=openai_key).bind_tools(tools)
+
+def adversary(prompt: str):
+    adversary_message = client.messages.create(
+    model="claude-haiku-4-5-20251001",
+    max_tokens=2000,
+    system = ''' You are a drug design assistant. You will recieve a proposal from  another model
+    of novel molecules it has designed to bind to a particular protein target. The proposal will 
+    include reasoning as to why the model thinks those molecules will bind well, and estimated 
+    docking scores for each molecule. Your task is to analyze the proposal and find any flaws 
+    in the reasoning or estimation of the docking scores. You should then suggest modifications 
+    to the proposed molecules that would make them more likely to bind well, and provide reasoning 
+    for why those modifications would help.
+    '''
+    messages=[
+        {"role": "user", "content": prompt},])
+
+    response = adversary_message.content[0].text
+
+    return response
+
+class State(TypedDict):
+  messages: Annotated[list, add_messages]
+
+def model_node(state: State) -> State:
+  res = model.invoke(state['messages'])
+  return {'messages': res}
+
+builder = StateGraph(State)
+builder.add_node('model', model_node)
+builder.add_node('tools', ToolNode(tools))
+builder.add_edge(START, 'model')
+builder.add_conditional_edges('model', tools_condition)
+builder.add_edge('tools',  'model')
+
+graph = builder.compile()
+
+sys_message = SystemMessage(content=f'''
+{task_specific_prompt}
+
+## You will first:
+- Read the list of molecule SMILES and scores
+- Ascertain any features of the molecules that contribute to the desired score. For example, if,
+from one molecule to the next, the addition of an O group makes the score better.
+- Gather all of these trends across all of the molecules.
+
+## If you need additional information to ascertain the trends, such as more modified
+molecules and their docking scores, you have tools you can call to generate new
+molecules and get their docking scores. You can use these tools as many times as you want
+to gather information on the trends.
+
+the tools include:
+                            
+- grow_cycle: starts with a molecule SMILES and adds substituents to it, docks them, and returns 
+              a list of molecules and scores. You can use this tool to further explore modifications
+              to promising molecules that you find in the input data. You can provide a list of
+              substituents to add, or use the predefined sets: e_withdraw (electron withdrawing),
+              e_donate (electron donating), withdraw_with_linkers (electron withdrawing with linkers), 
+              donate_with_linkers (electron donating with linkers). You can also generate a random list 
+              of substituents with the make_random_list tool and use that as input to grow_cycle.
+
+- replace_groups: starts with a molecule SMILES and replaces specific groups in it with new groups, returning a list of new
+                  molecules and scores. This tool allows you to test specific hypotheses about how replacing certain
+                  groups in a molecule might affect binding affinity. You can specify which groups to replace and
+                  what to replace them with, or use the predefined sets of substituents mentioned above. You can also 
+                  generate a random list of substituents with the make_random_list tool and use that as input to replace_groups.
+
+- make_random_list: this tool generates a list of substituents of specified length (num_items). It draws from the predefined lists:
+                    e_withdraw (electron withdrawing), e_donate (electron donating), withdraw_with_linkers 
+                    (electron withdrawing with linkers), donate_with_linkers (electron donating with linkers). 
+                    Use this tool when you want to get a broad sense of how different modifications affect binding affinity, 
+                    without having a specific hypothesis in mind.
+
+## Once you have ascertained the trends:
+- Use the trends you learned to suggest 1-5 new molecules that obey the trends you found
+and which should have a better score than the molecules in the list.
+- Provide reasoning as to why you created those new molecules.
+- Estimate the new scores.
+
+## You may ask te user for clarification if needed, but try to use the tools to gather as much information as you
+can before asking for clarification.
+''')
+
+global messages
+messages = [sys_message]
+
+#@spaces.GPU
+def start_chat():
+  '''
+  '''
+  global chat_history, messages, reasoning
+  chat_history = []
+  reasoning = []
+  messages = [sys_message]
+
+#@spaces.GPU
+def chat_turn(prompt: str):
+  '''
+  '''
+  global chat_history, messages, reasoning
+  human_message = HumanMessage(content=prompt)
+  messages.append(human_message)
+  local_history = [prompt]
+
+  input = {
+      'messages' : messages
+  }
+
+  for c in graph.stream(input):
+    try:
+      ai_mes = c['model']['messages'].content
+      messages.append(AIMessage(ai_mes))
+      if ai_mes != '':
+        #print(f'message is {ai_mes}')
+        local_history.append(ai_mes)
+    except:
+      pass
+    try:
+      if os.path.exists('current_image.png'):
+        if os.path.getmtime('current_image.png') > time.time() - 30:
+          img = Image.open('current_image.png')
+        else:
+          img = None
+      else:
+        img = None
+    except:
+      img = None
+    try:
+      reasoning.append(c['tools']['messages'][0].content)
+    except:
+      pass
+
+  if len(local_history) != 2:
+    local_history.append('no message')
+
+  #chat_history.append({'role': 'user', 'content': local_history[0]})
+  #chat_history.append({'role': 'assistant', 'content': local_history[1]})
+  chat_history.append(local_history)
+  return '', img, chat_history
+
+def get_initial_prompt(protein):
+  '''
+  '''
+  scoring_args[1] = protein
+  random_list, excluded_list = make_random_list(10)
+  first_list = sub_cycle(random_list, scoring_args)
+  context = ''
+  for smiles, score in first_list:
+      context += f"{smiles}: {score}\n"
+
+  first_prompt = f'''
+  Here is a list of molecules and their docking scores:
+  {context}\n'''
+
+  blank, img, mes = chat_turn(first_prompt)
+  #print(mes[-1])
+  
+  #chat_history.append({'role': 'user', 'content': 'User sent protein name'})
+  #chat_history.append({'role': 'assistant', 'content': 'Assistant running sub_cycle'})
+  #chat_history.append({'role': 'user', 'content': first_prompt})
+  #chat_history.append({'role': 'assistant', 'content': mes[-1]})
+  chat_history.append(['User sent protein name', 'Assistant running sub_cycle'])
+  chat_history.append([first_prompt, mes[-1]])
+  return chat_history
+
+start_chat()
+
+response_list = get_initial_prompt('HMGCR')
+with open('adversary_design.md', 'a') as f:
+    f.write(response_list[-1][-1]+'\n')
+
+ant_response = adversary(response_list[-1][-1])
+with open('adversary_design.md', 'a') as f:
+    f.write(ant_response+'\n')
+
+_, _, response_list = chat_turn(ant_response)
+with open('adversary_design.md', 'a') as f:
+    f.write(response_list[-1][-1]+'\n')
