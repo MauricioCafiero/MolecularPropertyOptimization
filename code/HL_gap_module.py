aafa8bf9b52c9c63009e16fe338a05d34c995baa
Mauricio Cafiero <155150383+MauricioCafiero@users.noreply.github.com>
2026-03-26 10:16:39 +0000

    Implement try-except for gap calculation
    Added error handling to HOMO-LUMO gap calculation.

--- code/HL_gap_module.py
+++ code/HL_gap_module.py
@@ -24,47 +24,51 @@ def scoring_function(smiles: str):
       gap: HOMO-LUMO gap in eV
       None: None
   '''
-  atoms_list = ""
-  mol = Chem.MolFromSmiles(smiles)
-  molH = Chem.AddHs(mol)
-  AllChem.EmbedMolecule(molH)
-  AllChem.MMFFOptimizeMolecule(molH)
-  xyz_string = f""
-  for atom in molH.GetAtoms():
-    atoms_list += atom.GetSymbol()
-    pos = molH.GetConformer().GetAtomPosition(atom.GetIdx())
-    xyz_string += f"{atom.GetSymbol()} {pos[0]} {pos[1]} {pos[2]}; "
-
-  charge = 0
-  for i,char in enumerate(smiles):
-    if char == '-' and smiles[i+1] != 'c':
-      charge -= 1
-    elif char == '+':
-      charge += 1
-
-  new_mol = gto.M(
-    atom  = xyz_string,
-    charge = charge,
-    spin = 0,
-    basis = 'sto-3g',
-    symmetry= True)
-
-  dft_test = dft.RKS(new_mol)
-  dft_test.xc = scoring_args[1]
-
-  start = time.time()
-  energy = dft_test.kernel()
-  end = time.time()
-
-  for i, occ in enumerate(dft_test.mo_occ):
-    if occ == 0:
-      homo_n = i-1
-      lumo_n = i
-      gap = (dft_test.mo_energy[lumo_n] - dft_test.mo_energy[homo_n])*27.21
-      break
-
-  total_time = end-start
-  print(f'The HOMO-LUMO gap for {smiles} is: {gap:.3f} and took {total_time:.3f} seconds')
+  try:
+    atoms_list = ""
+    mol = Chem.MolFromSmiles(smiles)
+    molH = Chem.AddHs(mol)
+    AllChem.EmbedMolecule(molH)
+    AllChem.MMFFOptimizeMolecule(molH)
+    xyz_string = f""
+    for atom in molH.GetAtoms():
+      atoms_list += atom.GetSymbol()
+      pos = molH.GetConformer().GetAtomPosition(atom.GetIdx())
+      xyz_string += f"{atom.GetSymbol()} {pos[0]} {pos[1]} {pos[2]}; "
+  
+    charge = 0
+    for i,char in enumerate(smiles):
+      if char == '-' and smiles[i+1] != 'c':
+        charge -= 1
+      elif char == '+':
+        charge += 1
+  
+    new_mol = gto.M(
+      atom  = xyz_string,
+      charge = charge,
+      spin = 0,
+      basis = 'sto-3g',
+      symmetry= True)
+  
+    dft_test = dft.RKS(new_mol)
+    dft_test.xc = scoring_args[1]
+  
+    start = time.time()
+    energy = dft_test.kernel()
+    end = time.time()
+  
+    for i, occ in enumerate(dft_test.mo_occ):
+      if occ == 0:
+        homo_n = i-1
+        lumo_n = i
+        gap = (dft_test.mo_energy[lumo_n] - dft_test.mo_energy[homo_n])*27.21
+        break
+  
+    total_time = end-start
+    print(f'The HOMO-LUMO gap for {smiles} is: {gap:.3f} and took {total_time:.3f} seconds')
+  except:
+    print('Could not calculate gap')
+    return 100.0, None
   
   return gap.item(), None
 
